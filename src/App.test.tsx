097303342a4267161872d25ac2a0774e2df4de78
Vlad Armizonov <wow-rotfl@yandex.ru>
2020-03-23 22:41:34 +0300

    Added Notifier, App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import App from './App'
+import store from './store'
+import { AuthProvider } from './contexts/AuthContext'
+
+afterAll(() => localStorage.setItem('isAuth', ''))
+
+const setup = () => (
+  render(
+    <AuthProvider store={store}>
+      <App />
+    </AuthProvider>
+  )
+)
 
 test('renders tt-odobrim app', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/tt-odobrim/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const { getByText } = setup()
+  const linkElement = getByText(/tt-odobrim/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('should show notify me form after login', () => {
+  const { getByText } = setup()
+  const loginBtn = getByText(/Log in/)
+  fireEvent.click(loginBtn)
+  expect(loginBtn.innerHTML).toBe('Log out')
+  expect(getByText(/Notify me if/)).toBeInTheDocument()
+})
--- src/components/Notifier/__tests__/Notifier.test.tsx
+++ src/components/Notifier/__tests__/Notifier.test.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import {
+  render,
+  fireEvent,
+  getByLabelText,
+  getByTestId,
+  findByTestId,
+} from '@testing-library/react'
 
 import Notifier from '../Notifier'
+import { ExchangeProvider } from '../../../contexts/ExchangeContext'
+import { ConditionOperator } from '../../../api/storeapi'
+
+afterAll(() => localStorage.setItem('notifications', ''))
 
 const getConvertionData = jest.fn()
   .mockReturnValue({
@@ -12,7 +22,11 @@ const getConvertionData = jest.fn()
   })
 
 function setup() {
-  const utils = render(<Notifier getConvertionData={getConvertionData} />)
+  const utils = render(
+    <ExchangeProvider>
+      <Notifier getConvertionData={getConvertionData} />
+    </ExchangeProvider>
+  )
 
   return {
     ...utils
@@ -26,6 +40,7 @@ function setupNotifyForm() {
   const exprOperatorSpan = utils.getByTestId('notifier-expr-operator') as HTMLSpanElement
   const curConversionInput = utils.getByTestId('notifier-current-conversion') as HTMLInputElement
   const cndnOperatorSelect = utils.getByTestId('notifier-condition-operator') as HTMLSelectElement
+  const notifySumInput = utils.getByTestId('notifier-notify-sum') as HTMLInputElement
   const notifyBtn = utils.getByText('Notify').parentNode as HTMLButtonElement
 
   return {
@@ -34,9 +49,43 @@ function setupNotifyForm() {
     exprOperatorSpan,
     curConversionInput,
     cndnOperatorSelect,
+    notifySumInput,
     notifyBtn,
   }
 }
+
+function createListItem(
+  changedToSum: string,
+  conditionOperator: ConditionOperator = 'gt'
+) {
+
+  const {
+    changedSumInput,
+    notifyBtn,
+    cndnOperatorSelect,
+    ...utils
+  } = setupNotifyForm()
+
+  fireEvent.change(changedSumInput, { target: { value: changedToSum } })
+  fireEvent.click(notifyBtn)
+  fireEvent.change(cndnOperatorSelect, { target: { value: conditionOperator } })
+
+  const regexpStr = `${changedToSum}\\(changed to\\)`
+  const regexp = new RegExp(regexpStr)
+  const itemLabel = utils.getByText(regexp)
+  const item = itemLabel.parentNode!.parentNode!.parentNode as HTMLLIElement
+
+  return {
+    ...utils,
+    item,
+  }
+}
+
+test('NotifierForm calculate notify sum', () => {
+  const { changedSumInput, notifySumInput } = setupNotifyForm()
+  fireEvent.change(changedSumInput, { target: { value: '100' } })
+  expect(notifySumInput.value).toBe('7100')
+})
 
 test('NotifierForm when change condition operator should change expression operator', () => {
   const { cndnOperatorSelect, exprOperatorSpan } = setupNotifyForm()
@@ -52,14 +101,26 @@ test('NotifierForm when changed amount filled notify btn should be active', () =
   expect(notifyBtn.hasAttribute('disabled')).toBeFalsy()
 })
 
-test('new notification should be in document', () => {
 
+test('New notification should be in document', () => {
+  const { item } = createListItem('100')
+  expect(item).toBeInTheDocument()
 })
 
-test('notification should have \'finish\' status', () => {
-
+test('Notification should have \'progress\' state', () => {
+  const { item } = createListItem('1000')
+  expect(getByTestId(item, 'notifier-list-item-progress')).toBeInTheDocument()
 })
 
-test('notification should be removed', () => {
+test('Notification should have \'success\' state', async () => {
+  const { item } = createListItem('1')
+  const successIcon = await findByTestId(item, 'notifier-list-item-success')
+  expect(successIcon).toBeInTheDocument()
+})
 
+test('Notification should be removed', () => {
+  const { item } = createListItem('52')
+  const deleteBtn = getByLabelText(item, 'delete')
+  fireEvent.click(deleteBtn)
+  expect(item).not.toBeInTheDocument()
 })
